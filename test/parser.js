1cf63ac14470c45698dc4bcdabfc0428038a5d11
Sergey Chikuyonok <serge.che@gmail.com>
2017-01-16 02:33:24 +0300

    Working parser implementation (WIP)

--- test/parser.js
+++ test/parser.js
@@ -2,10 +2,17 @@
 
 const assert = require('assert');
 require('babel-register');
-const parse = require('../lib/parser').default;
+const parser = require('../lib/parser').default;
 
 describe('CSS Abbreviation parser', () => {
+    const parse = abbr => parser(abbr).children[0];
+
+
     it('parse', () => {
-        console.log(parse('p10-20--30'));
+        // p10 -> [p, 10]
+        // p-10 -> [p, -10]
+        // p-10-20 -> [p, -10, 20]
+        // p-10--20 -> [p, -10, -20]
+        console.log(parse('p-10p-20--30'));
     });
 });
