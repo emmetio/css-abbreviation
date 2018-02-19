99b9665c3ad007d4b87822238c564aabbe47971c
Sergey Chikuyonok <serge.che@gmail.com>
2018-02-20 01:05:53 +0300

    Updated deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,12 @@
 export default {
-	entry: './index.js',
+	input: './index.js',
 	external: [
 		'@emmetio/stream-reader',
 		'@emmetio/stream-reader-utils',
 		'@emmetio/node'
 	],
-	targets: [
-		{format: 'cjs', dest: 'dist/css-abbreviation.cjs.js'},
-		{format: 'es',  dest: 'dist/css-abbreviation.es.js'}
+	output: [
+		{ format: 'cjs', file: 'dist/css-abbreviation.cjs.js' },
+		{ format: 'es',  file: 'dist/css-abbreviation.es.js' }
 	]
 };
